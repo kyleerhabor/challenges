0f04845f06293bbc82091dc9caebbe718c9f8bac
Kyle Erhabor <kyleerhabor@gmail.com>
2026-05-29 21:48:42 -0400

    Initial commit

---
+++ challenges/project-euler/0-problem-zero/clojure/src/kyleerhabor/project_euler_problem_zero/core.clj
@@ -0,0 +1,19 @@
+(ns kyleerhabor.project-euler-problem-zero.core)
+
+(defn square [x]
+  (* x x))
+
+(defn sum [xs]
+  (reduce + xs))
+
+(defn theorem [n]
+  (sum (filter odd? (map square (range 1 (inc n))))))
+
+(defn -main []
+  ;; A number is a perfect square, or a square number, if it is the square of a positive integer.
+  ;; For example, 25 is a square number because 5^2 = 5 * 5 = 25; it is also an odd square.
+  ;; 
+  ;; The first 5 square numbers are: 1, 4, 9, 16, 15, and the sum of the odd squares is 1 + 9 + 25.
+  ;; 
+  ;; Among the first 536 thousand square numbers, what is the sum of all the odd squares?
+  (theorem 536000))
