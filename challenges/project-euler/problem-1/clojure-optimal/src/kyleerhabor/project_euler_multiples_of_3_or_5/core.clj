878ac0ed30f053043c4662c1ea0bb6a8a132feb1
Kyle Erhabor <kyleerhabor@gmail.com>
2026-05-29 22:51:12 -0400

    Add Project Euler problem 1 optimal solution

--- challenges/project-euler/problem-1/clojure-optimal/src/kyleerhabor/project_euler_multiples_of_3_or_5/core.clj
+++ challenges/project-euler/problem-1/clojure-optimal/src/kyleerhabor/project_euler_multiples_of_3_or_5/core.clj
@@ -6,11 +6,11 @@
 (defn divides? [a b]
   (int? (/ b a)))
 
-(defn multiple? [n]
-  (or (divides? 3 n) (divides? 5 n)))
+(defn sums [n d]
+  (sum (filter #(divides? d %) (range 0 n d))))
 
 (defn theorem [n]
-  (sum (filter multiple? (range 1 n))))
+  (- (+ (sums n 3) (sums n 5)) (sums n 15)))
 
 (defn -main []
   ;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
@@ -19,5 +19,5 @@
   ;; Find the sum of all the multiples of 3 or 5 below 1000.
   (theorem 1000)
   (theorem 1000000)
-  ;; On my 2019 MacBook Pro, this takes well over a minute to evaluate.
+  ;; On my 2019 MacBook Pro, this takes 10-15 seconds to evaluate.
   #_(theorem 1000000000))
