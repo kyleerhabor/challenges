312e307b65aea085ef383c6cd7a310008e73d6ff
Kyle Erhabor <kyleerhabor@gmail.com>
2026-06-14 23:49:48 -0400

    Add Advent of Code 2025, day 3, part 2 solution

--- challenges/advent-of-code/2025/day-3/kotlin/src/main/kotlin/Main.kt
+++ challenges/advent-of-code/2025/day-3/kotlin/src/main/kotlin/Main.kt
@@ -13,12 +13,13 @@ data class Joltage(val high: Int, val low: Int) : Comparable<Joltage> {
     override fun compareTo(other: Joltage) = this.value.compareTo(other.value)
 }
 
-fun day1(input: String): String {
-    val banks = input
-        .trimEnd()
-        .lines()
-        .map { bank -> bank.map { it.digitToInt() } }
+fun banks(input: String): List<List<Int>> = input
+    .trimEnd()
+    .lines()
+    .map { bank -> bank.map { it.digitToInt() } }
 
+fun day1(input: String): String {
+    val banks = banks(input)
     val joltage = banks
         .map { bank -> // 4278
             val iterator = bank.listIterator()
@@ -40,8 +41,48 @@ fun day1(input: String): String {
     return joltage
 }
 
+private const val JOLTAGE_BATTERY_COUNT = 12
+
 fun day2(input: String): String {
-    TODO()
+    val banks = banks(input)
+    val joltage = banks
+        .map { bank ->
+            val iterator = bank.iterator()
+            val batteries = iterator.asSequence().take(JOLTAGE_BATTERY_COUNT).toList()
+            require(batteries.size == JOLTAGE_BATTERY_COUNT) { "Expected 12 joltage batteries" }
+
+            val joltage = iterator.asSequence().fold(batteries.toIntArray()) { joltage, battery ->
+                val candidate = joltage.toList() + battery
+                val index = when {
+                    candidate[0] < candidate[1] -> 0
+                    candidate[1] < candidate[2] -> 1
+                    candidate[2] < candidate[3] -> 2
+                    candidate[3] < candidate[4] -> 3
+                    candidate[4] < candidate[5] -> 4
+                    candidate[5] < candidate[6] -> 5
+                    candidate[6] < candidate[7] -> 6
+                    candidate[7] < candidate[8] -> 7
+                    candidate[8] < candidate[9] -> 8
+                    candidate[9] < candidate[10] -> 9
+                    candidate[10] < candidate[11] -> 10
+                    candidate[11] < candidate[12] -> 11
+                    else -> 12
+                }
+
+                val joltage = (candidate.slice(0 until index) + candidate.slice(index + 1..candidate.lastIndex))
+                    .toIntArray()
+
+                joltage
+            }
+
+            joltage
+        }
+        .sumOf { joltage ->
+            joltage.fold(0L) { a, battery -> a * 10 + battery }
+        }
+        .toString()
+
+    return joltage
 }
 
 class Cli : CliktCommand() {
@@ -51,7 +92,7 @@ class Cli : CliktCommand() {
         val input = this.input.readText()
 
         println("Day 1: ${day1(input = input)}")
-//        println("Day 2: ${day2(input = input)}")
+        println("Day 2: ${day2(input = input)}")
     }
 }
 
